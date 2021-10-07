c576d5addce258b1d70e919f2b480ea53443398a
Bartosz Los <bartosz.los@rtbhouse.com>
2021-10-07 17:59:15 +0200

    Model weights fixed

--- tests_performance/resources/benchmark.js
+++ tests_performance/resources/benchmark.js
@@ -1,7 +1,14 @@
+function randomValue() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random();
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
 function randomVector(n) {
     const vector = new Array(n);
     for (let i = 0; i < n; i++) {
-        vector[i] = Math.random();
+        vector[i] = randomValue();
     }
     return vector;
 }
