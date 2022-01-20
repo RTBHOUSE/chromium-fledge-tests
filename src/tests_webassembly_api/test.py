a052ade943fe0ac71cbcd7bc9d3913e3be04d08c
Bartosz Los <bartosz.los@rtbhouse.com>
2022-01-20 20:58:37 +0100

    Test case for a new WebAssembly API

---
+++ src/tests_webassembly_api/test.py
@@ -0,0 +1,40 @@
+# Copyright 2021 RTBHOUSE. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+import logging
+
+from common.base_test import BaseTest
+from common.mockserver import MockServer
+from common.utils import print_debug, measure_time, log_exception, MeasureDuration, pretty_json
+
+logger = logging.getLogger(__file__)
+
+
+class WebassemblyApiTest(BaseTest):
+
+    @print_debug
+    @measure_time
+    @log_exception
+    def test__basic_webassembly_api(self):
+        with MockServer(9031, '/home/usertd/tests/tests_webassembly_api/resources/buyer') as buyer_server,\
+                MockServer(9032, '/home/usertd/tests/tests_webassembly_api/resources/seller') as seller_server:
+
+            with MeasureDuration("joinAdInterestGroup"):
+                self.driver.get(buyer_server.address)
+                self.assertDriverContainsText('body', 'joined interest group')
+
+            with MeasureDuration("runAdAuction"):
+                self.driver.get(seller_server.address)
+                self.assertDriverContainsFencedFrame()
+
+        report_result_signals = seller_server.get_last_request("/reportResult").get_first_json_param('signals')
+        logger.info(f"reportResult() signals: {pretty_json(report_result_signals)}")
+
+        report_win_signals = buyer_server.get_last_request("/reportWin").get_first_json_param('signals')
+        logger.info(f"reportWin() signals: {pretty_json(report_win_signals)}")
+
+        # to be able to measure bidding worklet time you should use custom-built version of chromium
+        # with a patch like this: https://github.com/RTBHOUSE/chromium/commits/rtb_wasm
+        if 'bid_duration' in report_result_signals.get('browserSignals'):
+            bid_duration_ms = int(report_result_signals.get('browserSignals').get('bid_duration')) / 1000
+            logger.info(f"generateBid took: {bid_duration_ms} ms")
