3997547015c3427088641375d0930267d8413112
Maciej Kowalczyk <maciej.kowalczyk@rtbhouse.com>
2022-07-22 22:51:34 +0200

    Support for providing response contents in test definition + saving requests timestamps

--- src/common/mockserver/__init__.py
+++ src/common/mockserver/__init__.py
@@ -6,9 +6,11 @@ import logging
 import pathlib
 import ssl
 import threading
+import time
 from dataclasses import dataclass
 from functools import partial
-from typing import Dict, List, Optional
+from http import HTTPStatus
+from typing import Dict, List, Optional, Union, Iterator, Tuple
 from urllib.parse import parse_qs, urlsplit
 
 logger = logging.getLogger(__file__)
@@ -17,8 +19,9 @@ common_dir = str(pathlib.Path(__file__).absolute().parent.parent)
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
-    def __init__(self, *args, directory, callback, **kwargs):
-        self.callback = callback
+    def __init__(self, *args, directory=None, callback=None, response_provider=None, **kwargs):
+        self.callback = callback or (lambda request: None)
+        self.response_provider = response_provider or (lambda path, query_params: None)
         super().__init__(*args, directory=directory, **kwargs)
 
     def end_headers(self) -> None:
@@ -35,27 +38,47 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
             return False
         self.path, query = urlsplit(self.path)[2:4]
         self.query_params = parse_qs(query)
+        self.timestamp = time.time()
         logger.info(
             f"{self.command} request path: {self.path}, query params: {self.query_params}")
         return True
 
     def do_GET(self):
-        self.callback(Request(self.path, self.query_params))
-
-        if self.path.startswith("/report") or self.path.startswith("/debug") or self.path.startswith('/favicon'):
-            pass
+        self.callback(Request(self.path, self.query_params, self.timestamp))
+        response = self.response_provider(self.path, self.query_params)
+        response: Union[None, str, Tuple[int, Iterator[Tuple[str, str]], Union[None, str, bytes]]]
+        if response is None:
+            return super().do_GET()
+        elif isinstance(response, str):
+            response = HTTPStatus.OK, [], response
+        code, headers, body = response
+        self.send_response(code)
+        content_type_sent = False
+        for key, value in headers:
+            self.send_header(key, value)
+            if key.lower() == 'content-type':
+                content_type_sent = True
+        if body is None:
+            self.end_headers()
         else:
-            super().do_GET()
+            if isinstance(body, str):
+                body = body.encode()
+            if not content_type_sent:
+                self.send_header("Content-Type", self.guess_type(self.path))
+            self.send_header("Content-Length", str(len(body)))
+            self.end_headers()
+            self.wfile.write(body)
 
     def do_POST(self):
         body = self.rfile.read(int(self.headers['Content-Length']))
-        self.callback(Request(self.path, self.query_params, body))
+        self.callback(Request(self.path, self.query_params, self.timestamp, body))
 
 
 @dataclass(init=True, repr=True, eq=False, frozen=True)
 class Request:
     path: str
     params: Dict[str, List[str]]
+    timestamp: float
     body: Optional[bytes] = None
 
     def get_params(self, key):
@@ -69,18 +92,18 @@ class Request:
 
 
 class MockServer:
-    def __init__(self, port, directory):
+    def __init__(self, port=0, directory='.', response_provider=None):
         self.server_name = 'https://fledge-tests.creativecdn.net'
         self.server_port = port
         self.server_directory = directory
-        self.http_server = None
         self.requests = []
 
         logger.debug(f"server {self.address} initializing")
         server_address = ('0.0.0.0', self.server_port)
         self.http_server = http.server.ThreadingHTTPServer(
             server_address,
-            partial(RequestHandler, directory=self.directory, callback=self.requests.append))
+            partial(RequestHandler, directory=self.server_directory, response_provider=response_provider,
+                    callback=self.requests.append))
         self.server_port = self.http_server.server_port
         self.http_server.socket = ssl.wrap_socket(
             self.http_server.socket,
@@ -92,10 +115,6 @@ class MockServer:
     @property
     def address(self):
         return f'{self.server_name}:{self.server_port}'
-
-    @property
-    def directory(self):
-        return self.server_directory
 
     def __enter__(self):
         logger.debug(f"server {self.address} starting")
