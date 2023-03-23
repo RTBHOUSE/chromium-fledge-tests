dcf7e841cb6488e38beaba224e289c16763f0ff8
Fabian Höring <f.horing@criteo.com>
2023-03-23 14:30:19 +0000

    Use new headless chrome mode
    - https://bugs.chromium.org/p/chromium/issues/detail?id=1229652 has been fixed

--- src/common/base_test.py
+++ src/common/base_test.py
@@ -34,15 +34,12 @@ class BaseTest(unittest.TestCase):
         options = webdriver.ChromeOptions()
         if CHROMIUM_DIR:
             options.binary_location = CHROMIUM_DIR + '/chrome'
-        # FIXME headless chrome does not work with fledge, https://bugs.chromium.org/p/chromium/issues/detail?id=1229652
-        # options.headless = True
         options.set_capability('goog:loggingPrefs', dict(browser='ALL', performance='ALL'))
         options.add_experimental_option('perfLoggingPrefs', dict(traceCategories='fledge'))
         options.add_argument('--enable-stats-collection-bindings')  # for histograms
         options.add_argument('--no-sandbox')
         options.add_argument('--no-zygote')
-        # FIXME headless chrome does not work with fledge, https://bugs.chromium.org/p/chromium/issues/detail?id=1229652
-        # options.add_argument('--headless')
+        options.add_argument('--headless=new')
         options.add_argument('--disable-gpu')
         options.add_argument(f'--user-data-dir={PROFILE_DIR}')
         options.add_argument('--disable-features=ChromeWhatsNewUI')
