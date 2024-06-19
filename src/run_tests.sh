58e75e87098bfc780a3b820723e4abbadca8f559
Stanislaw Findeisen <stanislaw@ik.me>
2024-06-19 17:36:28 +0200

    ML-407 print more debug info

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -vx
-echo "hello from chromium-fledge-tests/src/run_tests.sh"
+echo "hello from chromium-fledge-tests/src/run_tests.sh; PYTHONPATH=$PYTHONPATH"
 pwd
 ls -laR
 python3 -m unittest $TEST --verbose
