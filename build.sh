9223bbc07913841b4d99b48ee2c9bd4df5fdff76
Stanislaw Findeisen <stanislaw@ik.me>
2024-06-20 07:15:06 +0200

    ML-407 remove unused file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -vx
-echo "hello from chromium-fledge-tests/build.sh"
-pwd
-
-docker build . -f Dockerfile.node --iidfile .iidfile.node
-
-docker run --rm `cat .iidfile.node` node tests_tensorflow/resources/buyer/build_buyer.js > tests_tensorflow/resources/buyer/buyer.js
